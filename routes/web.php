5f82d7e0c7dae28f34e182d512d904459dc84b80
denysmotion <denysmotion@gmail.com>
2025-01-04 15:36:31 +0100

    add admin lte

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController as MainController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/admin', [MainController::class, 'index'])->name('admin.dashboard');
