137fcbf44f9f82703e8358818e78ced0e8673555
denysmotion <denysmotion@gmail.com>
2025-01-07 17:22:57 +0100

    feat: post soft deleting

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    use Sluggable;
+    use Sluggable, SoftDeletes;
 
     protected $fillable = ['title', 'meta_desc', 'content', 'category_id', 'thumb'];
 
