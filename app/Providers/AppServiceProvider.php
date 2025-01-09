5e5483fdff5187b83534bb4223af434d972e5af1
denysmotion <denysmotion@gmail.com>
2025-01-09 11:49:53 +0100

    add popular posts to sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View as ViewFacade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        view()->composer('blog.layouts.incs.sidebar', function(View $view) {
+            $view->with('popular_posts', Post::query()->orderBy('views', 'desc')->limit(3)->get());
+        });
+
+        // ViewFacade::composer('blog.layouts.incs.sidebar', function(View $view) {
+        //     $view->with('popular_posts', Post::query()->orderBy('views', 'desc')->limit(3)->get());
+        // });
     }
 }
